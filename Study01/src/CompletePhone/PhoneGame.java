2aaee7c134a34465cee3c0b6b26f21a7415a8c13
seonmin1 <iseonmin161@gmail.com>
2024-10-30 22:39:47 +0900

    Delete Study01 directory

--- Study01/src/CompletePhone/PhoneGame.java
+++
@@ -1,8 +0,0 @@
-package CompletePhone;
-
-public abstract class PhoneGame extends PhoneCalculator {
-    @Override
-    public void game() {
-        System.out.println("게임 메뉴를 실행합니다.");
-    }
-}
--- Study01/src/SimplePhone/PhoneGame.java
+++
@@ -1,8 +0,0 @@
-package SimplePhone;
-
-public abstract class PhoneGame implements Phone {
-    @Override
-    public void game() {
-        System.out.println("게임 메뉴를 실행합니다.");
-    }
-}
